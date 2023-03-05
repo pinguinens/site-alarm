6e7a46f9a837f4983d57d70fe5d2bc54237dac03
Nikolay Barabanov <nbarabanov@mtt.ru>
2023-03-05 22:41:58 +0300

    tcp messenger server

--- cmd/service/main.go
+++ cmd/service/main.go
@@ -1,10 +1,10 @@
 package main
 
 import (
+	"bufio"
+	"net"
+
 	"github.com/rs/zerolog/log"
-	"golang.org/x/net/websocket"
-	"io"
-	"net/http"
 )
 
 const (
@@ -12,32 +12,56 @@ const (
 )
 
 func main() {
-	http.Handle("/receive", websocket.Handler(ReceiveHandler))
-	http.HandleFunc("/alarm", AlarmHandler)
-	err := http.ListenAndServe("localhost:8080", nil)
-	if err != nil {
-		panic("ListenAndServe: " + err.Error())
-	}
-}
-
-func ReceiveHandler(ws *websocket.Conn) {
-	for {
-		var bytes []byte
-		if err := websocket.Message.Receive(ws, &bytes); err != nil {
-			log.Err(err)
-			return
-		}
-
-		log.Info().Bytes("input", bytes).Send()
-	}
-}
-
-func AlarmHandler(w http.ResponseWriter, r *http.Request) {
-	bytes, err := io.ReadAll(r.Body)
+	// TODO: tcp messenger server
+	ln, err := net.Listen("tcp", ":8081")
 	if err != nil {
 		log.Err(err)
-		return
 	}
-
-	log.Info().Bytes("input", bytes).Send()
+	for {
+		conn, err := ln.Accept()
+		if err != nil {
+			log.Err(err)
+		}
+		go handleConnection(conn)
+	}
 }
+
+func handleConnection(conn net.Conn) {
+	for {
+		message, _ := bufio.NewReader(conn).ReadString('\n')
+		log.Info().Str("input", message).Send()
+		newmessage := "OK"
+		conn.Write([]byte(newmessage + "\n"))
+	}
+}
+
+//func main() {
+//	http.Handle("/receive", websocket.Handler(ReceiveHandler))
+//	http.HandleFunc("/alarm", AlarmHandler)
+//	err := http.ListenAndServe("localhost:8080", nil)
+//	if err != nil {
+//		panic("ListenAndServe: " + err.Error())
+//	}
+//}
+//
+//func ReceiveHandler(ws *websocket.Conn) {
+//	for {
+//		var bytes []byte
+//		if err := websocket.Message.Receive(ws, &bytes); err != nil {
+//			log.Err(err)
+//			return
+//		}
+//
+//		log.Info().Bytes("input", bytes).Send()
+//	}
+//}
+//
+//func AlarmHandler(w http.ResponseWriter, r *http.Request) {
+//	bytes, err := io.ReadAll(r.Body)
+//	if err != nil {
+//		log.Err(err)
+//		return
+//	}
+//
+//	log.Info().Bytes("input", bytes).Send()
+//}
