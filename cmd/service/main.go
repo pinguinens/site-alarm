f5e6f9da67245b3964aa0fbeeaaab7c64648412f
Nikolay Barabanov <nbarabanov@mtt.ru>
2023-03-05 17:42:20 +0300

    base websocket server

--- cmd/service/main.go
+++ cmd/service/main.go
@@ -1,3 +1,33 @@
 package main
 
-func main() {}
+import (
+	"net/http"
+
+	"golang.org/x/net/websocket"
+
+	"github.com/rs/zerolog/log"
+)
+
+const (
+	appVersion = "0.1"
+)
+
+func main() {
+	http.Handle("/alarm", websocket.Handler(Server))
+	err := http.ListenAndServe("localhost:8080", nil)
+	if err != nil {
+		panic("ListenAndServe: " + err.Error())
+	}
+}
+
+func Server(ws *websocket.Conn) {
+	for {
+		var bytes []byte
+		if err := websocket.Message.Receive(ws, &bytes); err != nil {
+			log.Err(err)
+			return
+		}
+
+		log.Info().Bytes("input", bytes).Send()
+	}
+}
--- go.mod
+++ go.mod
@@ -1,3 +1,14 @@
 module github.com/pinguinens/site-alarm
 
 go 1.19
+
+require (
+	github.com/rs/zerolog v1.29.0
+	golang.org/x/net v0.8.0
+)
+
+require (
+	github.com/mattn/go-colorable v0.1.13 // indirect
+	github.com/mattn/go-isatty v0.0.17 // indirect
+	golang.org/x/sys v0.6.0 // indirect
+)
